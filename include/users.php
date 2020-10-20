1fc4f61acd5143b07e3a6e90590831ea4578efee
kc1awv <smiller@kc1awv.net>
2020-10-20 17:22:06 -0400

    new icon pack

--- include/users.php
+++ include/users.php
@@ -130,7 +130,7 @@ for ($i=0;$i<$Reflector->StationCount();$i++) {
 
         list ($Flag, $Name) = $Reflector->GetFlag($Reflector->Stations[$i]->GetCallSign());
         if (file_exists("./images/flags/" . $Flag . ".png")) {
-            echo '<a href="#" class="tip"><img src="./images/flags/' . $Flag . '.png" class="table-flag" alt="' . $Name . '"><span>' . $Name . '</span></a>';
+            echo '<a href="#" class="tip"><img src="./images/flags/' . $Flag . '.svg" class="table-flag" alt="' . $Name . '" width="32"><span>' . $Name . '</span></a>';
         }
         echo '</td>
    <td><a href="https://www.qrz.com/db/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank">' . $Reflector->Stations[$i]->GetCallsignOnly() . '</a></td>
