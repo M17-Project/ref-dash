4991d18a6a247539696345aad253344aa4323289
kc1awv <smiller@kc1awv.net>
2020-10-22 11:39:11 -0400

    navbar fixes

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ if ($CallingHome['Active']) {
 <?php if (file_exists("./tracking.php")) {
     include_once("tracking.php");
 } ?>
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="#"><?php echo $Reflector->GetReflectorName(); ?> Reflector</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
