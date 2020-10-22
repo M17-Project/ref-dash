bd06ee65b840e33df00163cce482142cc0245978
kc1awv <smiller@kc1awv.net>
2020-10-22 11:13:46 -0400

    users fixes

--- include/users.php
+++ include/users.php
@@ -90,118 +90,98 @@ if (isset($_GET['do'])) {
             <th>Last heard</th>
             <th><img src="./images/ear.png" alt="Listening on" /></th>
          </tr>
-<?php
-
-$Reflector->LoadFlags();
-for ($i=0;$i<$Reflector->StationCount();$i++) {
-    $ShowThisStation = true;
-    if ($PageOptions['UserPage']['ShowFilter']) {
-        $CS = true;
-        if ($_SESSION['FilterCallSign'] != null) {
-            if (!fnmatch($_SESSION['FilterCallSign'], $Reflector->Stations[$i]->GetCallSign(), FNM_CASEFOLD)) {
-                $CS = false;
+        <?php
+            $Reflector->LoadFlags();
+            for ($i=0;$i<$Reflector->StationCount();$i++) {
+                $ShowThisStation = true;
+                if ($PageOptions['UserPage']['ShowFilter']) {
+                    $CS = true;
+                        if ($_SESSION['FilterCallSign'] != null) {
+                            if (!fnmatch($_SESSION['FilterCallSign'], $Reflector->Stations[$i]->GetCallSign(), FNM_CASEFOLD)) {
+                                $CS = false;
+                            }
+                        }
+                    $MO = true;
+                    if ($_SESSION['FilterModule'] != null) {
+                        if (trim(strtolower($_SESSION['FilterModule'])) != strtolower($Reflector->Stations[$i]->GetModule())) {
+                            $MO = false;
+                        }
+                    }
+                    $ShowThisStation = ($CS && $MO);
+                }
+                if ($ShowThisStation) {
+                    echo '
+                        <tr class="table-center">
+                            <td>';
+                                if ($i == 0 && $Reflector->Stations[$i]->GetLastHeardTime() > (time() - 60)) {
+                                    echo '<img src="./images/tx.gif" style="margin-top:3px;" height="20"/>';
+                                } else {
+                                    echo($i + 1);
+                                }
+                            echo '</td>
+                            <td>';
+                                list ($Flag, $Name) = $Reflector->GetFlag($Reflector->Stations[$i]->GetCallSign());
+                                if (file_exists("./images/flags/" . $Flag . ".svg")) {
+                                    echo '<a href="#" class="tip"><img src="./images/flags/' . $Flag . '.svg" class="table-flag" alt="' . $Name . '"><span>' . $Name . '</span></a>';
+                                }
+                            echo '</td>
+                            <td><a href="https://www.qrz.com/db/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank">' . $Reflector->Stations[$i]->GetCallsignOnly() . '</a></td>
+                            <td>' . $Reflector->Stations[$i]->GetSuffix() . '</td>
+                            <td><a href="http://www.aprs.fi/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank"><img src="./images/sat.png" alt=""></a></td>
+                            <td>' . $Reflector->Stations[$i]->GetVia();
+                            if ($Reflector->Stations[$i]->GetPeer() != $Reflector->GetReflectorName()) {
+                                echo ' / ' . $Reflector->Stations[$i]->GetPeer();
+                            }
+                            echo '</td>
+                            <td>' . @date("d.m.Y H:i", $Reflector->Stations[$i]->GetLastHeardTime()) . '</td>
+                            <td>' . $Reflector->Stations[$i]->GetModule() . '</td>
+                        </tr>';
+                }
+                if ($i == $PageOptions['LastHeardPage']['LimitTo']) {
+                    $i = $Reflector->StationCount() + 1;
+                }
             }
-        }
-        $MO = true;
-        if ($_SESSION['FilterModule'] != null) {
-            if (trim(strtolower($_SESSION['FilterModule'])) != strtolower($Reflector->Stations[$i]->GetModule())) {
-                $MO = false;
-            }
-        }
-
-        $ShowThisStation = ($CS && $MO);
-    }
-
-    if ($ShowThisStation) {
-
-        echo '
-      <tr class="table-center">
-       <td>';
-        if ($i == 0 && $Reflector->Stations[$i]->GetLastHeardTime() > (time() - 60)) {
-            echo '<img src="./images/tx.gif" style="margin-top:3px;" height="20"/>';
-        } else {
-            echo($i + 1);
-        }
-
-
-        echo '</td>
-        <td>';
-
-        list ($Flag, $Name) = $Reflector->GetFlag($Reflector->Stations[$i]->GetCallSign());
-        if (file_exists("./images/flags/" . $Flag . ".svg")) {
-            echo '<a href="#" class="tip"><img src="./images/flags/' . $Flag . '.svg" class="table-flag" alt="' . $Name . '"><span>' . $Name . '</span></a>';
-        }
-        echo '</td>
-   <td><a href="https://www.qrz.com/db/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank">' . $Reflector->Stations[$i]->GetCallsignOnly() . '</a></td>
-   <td>' . $Reflector->Stations[$i]->GetSuffix() . '</td>
-   <td><a href="http://www.aprs.fi/' . $Reflector->Stations[$i]->GetCallsignOnly() . '" class="pl" target="_blank"><img src="./images/sat.png" alt=""></a></td>
-   <td>' . $Reflector->Stations[$i]->GetVia();
-        if ($Reflector->Stations[$i]->GetPeer() != $Reflector->GetReflectorName()) {
-            echo ' / ' . $Reflector->Stations[$i]->GetPeer();
-        }
-        echo '</td>
-   <td>' . @date("d.m.Y H:i", $Reflector->Stations[$i]->GetLastHeardTime()) . '</td>
-   <td>' . $Reflector->Stations[$i]->GetModule() . '</td>
- </tr>';
-    }
-    if ($i == $PageOptions['LastHeardPage']['LimitTo']) {
-        $i = $Reflector->StationCount() + 1;
-    }
-}
-
-?> 
- 
+        ?>
     </table>
+</div>
 
-   <div class="col-md-3">
-      <a href="https://m17project.org"><img class="hidden-xs hidden-sm" src="./images/m17_logo.svg" width="100%"></a>
-      <table class="table table-striped table-hover">
-         <?php 
-
-$Modules = $Reflector->GetModules();
-sort($Modules, SORT_STRING);
-echo '<tr>';
-for ($i=0;$i<count($Modules);$i++) {
-   
-   if (isset($PageOptions['ModuleNames'][$Modules[$i]])) {
-      echo '<th>'.$PageOptions['ModuleNames'][$Modules[$i]];
-       if (trim($PageOptions['ModuleNames'][$Modules[$i]]) != "") {
-           echo '<br />';
-       }
-      echo $Modules[$i].'</th>';
-   }
-   else {
-   echo '
-  
-      <th>'.$Modules[$i].'</th>';
-   }
-}
-
-echo '</tr><tr>';
-
-$GlobalPositions = array();
-
-for ($i=0;$i<count($Modules);$i++) {
-    
-   $Users = $Reflector->GetNodesInModulesByID($Modules[$i]);
-   echo '<td><table class="table table-hover">';
-
-   $UserCheckedArray = array();
-   
-   for ($j=0;$j<count($Users);$j++) {
-       $Displayname = $Reflector->GetCallsignAndSuffixByID($Users[$j]);
-      echo '
-            <tr>
-               <td><a href="http://www.aprs.fi/'.$Displayname.'" class="pl" target="_blank">'.$Displayname.'</a> </td>
-            </tr>';
-      $UserCheckedArray[] = $Users[$j];
-   }
-   echo '</table></td>';
-}
-
-echo '</tr>';
-
-?>
-      </table>
-   </div>
+<div class="col-md-3">
+    <a href="https://m17project.org"><img class="hidden-xs hidden-sm" src="./images/m17_logo.svg" width="100%"></a>
+    <table class="table table-striped table-hover">
+        <?php 
+            $Modules = $Reflector->GetModules();
+            sort($Modules, SORT_STRING);
+            echo '<tr>';
+            for ($i=0;$i<count($Modules);$i++) {
+                if (isset($PageOptions['ModuleNames'][$Modules[$i]])) {
+                    echo '<th>'.$PageOptions['ModuleNames'][$Modules[$i]];
+                    if (trim($PageOptions['ModuleNames'][$Modules[$i]]) != "") {
+                        echo '<br />';
+                    }
+                echo $Modules[$i].'</th>';
+                } else {
+                    echo '
+                    <th>'.$Modules[$i].'</th>';
+                }
+            }
+            echo '</tr><tr>';
+            $GlobalPositions = array();
+            for ($i=0;$i<count($Modules);$i++) {
+                $Users = $Reflector->GetNodesInModulesByID($Modules[$i]);
+                echo '<td><table class="table table-hover">';
+                $UserCheckedArray = array();
+                for ($j=0;$j<count($Users);$j++) {
+                    $Displayname = $Reflector->GetCallsignAndSuffixByID($Users[$j]);
+                    echo '
+                        <tr>
+                            <td><a href="http://www.aprs.fi/'.$Displayname.'" class="pl" target="_blank">'.$Displayname.'</a> </td>
+                        </tr>';
+                        $UserCheckedArray[] = $Users[$j];
+                }
+                echo '</table></td>';
+            }
+            echo '</tr>';
+        ?>
+    </table>
+</div>
 </div>
