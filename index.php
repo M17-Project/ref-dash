433e059643edf56fc92e84a89e0eb7a20e6ad5d9
kc1awv <smiller@kc1awv.net>
2020-10-22 08:57:22 -0400

    navbar css

--- index.php
+++ index.php
@@ -104,6 +104,7 @@ if ($CallingHome['Active']) {
 
     <!-- Custom styles for this template -->
     <link href="css/dashboard.css" rel="stylesheet">
+    <link href="css/navbar-top-fixed.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
