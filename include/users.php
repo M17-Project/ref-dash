348944d59e91e2e963d94908b8e0cf88f285869a
kc1awv <smiller@kc1awv.net>
2020-10-24 14:47:11 -0400

    add elapsed time function

--- include/users.php
+++ include/users.php
@@ -91,6 +91,27 @@ if (isset($_GET['do'])) {
             <th><img src="./images/ear.png" alt="Listening on" /></th>
          </tr>
         <?php
+            function elapsedTime ($time){
+            
+                $time = time() - $time; // to get the time since that moment
+                $time = ($time<1)? 1 : $time;
+                $tokens = array (
+                    31536000 => 'year',
+                    2592000 => 'month',
+                    604800 => 'week',
+                    86400 => 'day',
+                    3600 => 'hour',
+                    60 => 'minute',
+                    1 => 'second'
+                );
+            
+                foreach ($tokens as $unit => $text) {
+                    if ($time < $unit) continue;
+                    $numberOfUnits = floor($time / $unit);
+                    return $numberOfUnits.' '.$text.(($numberOfUnits>1)?'s':'');
+                }
+            
+            }
             $Reflector->LoadFlags();
             for ($i=0;$i<$Reflector->StationCount();$i++) {
                 $ShowThisStation = true;
